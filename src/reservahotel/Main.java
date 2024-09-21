2594a200e2a5813b9849b8089616d95a6c91d560
rian-mm647 <rian.mm647@gmail.com>
2024-09-21 11:01:20 -0300

    Update Main.java
    Fiz o menu de nova reserva

--- src/reservahotel/Main.java
+++ src/reservahotel/Main.java
@@ -5,13 +5,28 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.print("-=-=-=-=-= Sistema de Reserva de Hotéis -=-=-=-=-=");
+		System.out.print("\n-=-=-=-=-= Sistema de Reserva de Hotéis =-=-=-=-=-");
 		int opcao = 0;
 		do {
-			System.out.print("Bem vindo");
+			System.out.println("\n\nBem vindo, selecione uma das opções abaixo para prosseguir:");
+			System.out.println("1 - Iniciar reserva");
+			System.out.println("2 - Cancelar uma reserva");
+			System.out.println("3 - Sair");
+			System.out.println("Opção desejada: ");
+			opcao = scanner.nextInt();
 			switch (opcao) {
-				
+				case 1:
+					Hospede novoHospede = new Hospede();
+					System.out.println("Insira suas informações: ");
+					novoHospede.getNome();
+					novoHospede.getCpf();
+					novoHospede.getNascimento();
+					break;
+
+				case 2:
+					System.out.println("Digite o código da reserva que deseja cancelar: ");
 			}
-		} while (opcao != 6);
+		} while (opcao != 3);
+		scanner.close();
 	}
 }
