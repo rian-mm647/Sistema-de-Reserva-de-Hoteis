4a2d58721937b5d20c4fb2f60061591a0a49f6b4
rian-mm647 <rian.mm647@gmail.com>
2024-09-21 11:00:41 -0300

    Update Hospede.java
    Construi os métodos get

--- src/reservahotel/Hospede.java
+++ src/reservahotel/Hospede.java
@@ -1,23 +1,40 @@
 package reservahotel;
 
+import java.util.Scanner;
+
 public class Hospede {
 	public String nome;
 	public String cpf;
 	public String nascimento;
 	
 	public String getNome() {
+		String nome;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Nome: ");
+		nome = scanner.nextLine();
+		scanner.close();
 		return nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 	public String getCpf() {
+		String cpf;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("CPF: ");
+		cpf = scanner.nextLine();
+		scanner.close();
 		return cpf;
 	}
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
 	public String getNascimento() {
+		String nascimento;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Data de nascimento: ");
+		nascimento = scanner.nextLine();
+		scanner.close();
 		return nascimento;
 	}
 	public void setNascimento(String nascimento) {
